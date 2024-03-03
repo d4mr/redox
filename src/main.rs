7c1752b7cbbf3efa7f02e771d3c5c10fa64fe71a
Prithvish Baidya <deformercoding@gmail.com>
2024-03-03 21:50:08 +0530

    Convert to async

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,45 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-    str,
+use std::str;
+
+use tokio::{
+    io::AsyncWriteExt,
+    net::{TcpListener, TcpStream},
 };
+
+enum Command {
+    Ping,
+}
 
 const INTERFACE: &str = "127.0.0.1";
 const PORT: &str = "6379";
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Starting Server on {} at port {}", INTERFACE, PORT);
-
-    let listener = TcpListener::bind(format!("{}:{}", INTERFACE, PORT)).unwrap();
+    let listener = TcpListener::bind(format!("{}:{}", INTERFACE, PORT))
+        .await
+        .unwrap();
 
     println!("Listening at {}", listener.local_addr().unwrap());
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 1024];
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        process(stream).await;
+    }
+}
 
-                println!("accepted new connection");
-                loop {
-                    let n = stream.read(&mut buf).expect("Could not read from stream");
-                    if n == 0 {
-                        continue;
-                    }
+async fn process(mut stream: TcpStream) {
+    handle_command(Command::Ping, &mut stream).await;
+}
 
-                    let s = match str::from_utf8(&buf) {
-                        Ok(v) => v,
-                        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-                    };
-
-                    stream
-                        .write_all("+PONG\r\n".as_bytes())
-                        .expect("could not write to buffer");
-
-                    println!("Got message from connection: {}", s);
-                    println!("Responding with: pong");
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+async fn handle_command(command: Command, stream: &mut TcpStream) {
+    match command {
+        Command::Ping => {
+            stream
+                .write_all("+PONG\r\n".as_bytes())
+                .await
+                .expect("could not write to buffer");
+            println!("Responding with: pong");
         }
     }
 }
